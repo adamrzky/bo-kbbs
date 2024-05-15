a592848d289f0d111b440532e480bea80fd22ff1
adamrzky <adamrizky340@gmail.com>
2024-05-15 11:59:25 +0700

    fix

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -23,11 +23,11 @@ class TransactionController extends Controller
      */
     function __construct()
     {
-         $this->middleware('permission:transaction-list|transaction-create|transaction-edit|transaction-delete', ['only' => ['index','show']]);
-         $this->middleware('permission:transaction-create', ['only' => ['create','store']]);
-         $this->middleware('permission:transaction-edit', ['only' => ['edit','update']]);
-         $this->middleware('permission:transaction-delete', ['only' => ['destroy']]);
-         $this->middleware('permission:transaction-broadcast', ['only' => ['broadcast']]);
+        $this->middleware('permission:transaction-list|transaction-create|transaction-edit|transaction-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:transaction-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:transaction-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:transaction-delete', ['only' => ['destroy']]);
+        $this->middleware('permission:transaction-broadcast', ['only' => ['broadcast']]);
     }
     /**
      * Display a listing of the resource.
@@ -42,9 +42,9 @@ class TransactionController extends Controller
     public function index()
     {
         // $data = Http::get('http://192.168.26.26:10002/tm.php')->json();
-        
+
         // foreach ($data as $key => $value) {
-          
+
         //     $merchant = Merchant::where('ID',$value['MERCHANT_ID'])->first();
         //     if ($merchant != '') {
         //         // dd($merchant);
@@ -53,10 +53,10 @@ class TransactionController extends Controller
         //         $data[$key]['MERCHANT'] = $merchant;
         //     }
 
-            
-           
+
+
         // }
-       
+
         // dd($data);
         return view('transactions.index');
     }
@@ -64,104 +64,151 @@ class TransactionController extends Controller
     public function data(Request $request)
     {
 
-  
+
 
 
         if ($request->ajax()) {
 
+
+
+
+            switch (env('APP_ENV')) {
+                case 'local':
+                    $data = Http::get('http://192.168.26.26:10002/tm.php')->json();
+
+                    foreach ($data as $key => $value) {
+
+                        $merchant = Merchant::where('ID', $value['MERCHANT_ID'])->first();
+                        if ($merchant != '') {
+                            // dd($merchant);
+                            $data[$key]['MERCHANT'] = $merchant->toArray();
+                        } else {
+                            $data[$key]['MERCHANT'] = $merchant;
+                        }
+                    }
+                    break;
+                case 'dev':
+                    $data = Http::get('http://192.168.26.26:10002/tm.php')->json();
+
+                    foreach ($data as $key => $value) {
+
+                        $merchant = Merchant::where('ID', $value['MERCHANT_ID'])->first();
+                        if ($merchant != '') {
+                            // dd($merchant);
+                            $data[$key]['MERCHANT'] = $merchant->toArray();
+                        } else {
+                            $data[$key]['MERCHANT'] = $merchant;
+                        }
+                    }
+                    break;
+                case 'prod':
+                    $getUserId = Auth::id();
+                    $userId = $getUserId;
+
+                    $query = DB::table('QRIS_TRANSACTION_AQUERIER_MAIN')
+                        ->join('user_has_merchant', 'QRIS_TRANSACTION_AQUERIER_MAIN.MERCHANT_ID', '=', 'user_has_merchant.MERCHANT_ID')
+                        ->join('users', 'user_has_merchant.USER_ID', '=', 'users.id')
+                        ->select('QRIS_TRANSACTION_AQUERIER_MAIN.*');
+
+                    if ($userId != 1) {
+                        $query->where('users.id', $userId);
+                    }
+
+                    // dd($query->get()->toArray());
+
+                    $data = $query->get()->toArray();
+                    break;
+            }
+
+
+
+
             // $data = Transaction::get()->toArray();
 
-            $getUserId = Auth::id();
-            $userId = $getUserId;
-    
-            $query = DB::table('QRIS_TRANSACTION_AQUERIER_MAIN')
-            ->join('user_has_merchant', 'QRIS_TRANSACTION_AQUERIER_MAIN.MERCHANT_ID', '=', 'user_has_merchant.MERCHANT_ID')
-            ->join('users', 'user_has_merchant.USER_ID', '=', 'users.id')
-            ->select('QRIS_TRANSACTION_AQUERIER_MAIN.*');
-        
-            if ($userId != 1) {
-                $query->where('users.id', $userId);
-            }
-    
-            // dd($query->get()->toArray());
-
-            $data = $query->get()->toArray();
 
 
-       
 
-        // $data = Http::get('http://192.168.26.26:10002/tm.php')->json();
-        
-        // foreach ($data as $key => $value) {
-          
-        //     $merchant = Merchant::where('ID',$value['MERCHANT_ID'])->first();
-        //     if ($merchant != '') {
-        //         // dd($merchant);
-        //         $data[$key]['MERCHANT'] = $merchant->toArray();
-        //     } else {
-        //         $data[$key]['MERCHANT'] = $merchant;
-        //     }
 
-            
-           
-        // }
-       
-      
 
-        // dd($data);
-        return DataTables::of($data)
-    
-        ->toJson();
+            // $data = Http::get('http://192.168.26.26:10002/tm.php')->json();
 
-        
+            // foreach ($data as $key => $value) {
+
+            //     $merchant = Merchant::where('ID',$value['MERCHANT_ID'])->first();
+            //     if ($merchant != '') {
+            //         // dd($merchant);
+            //         $data[$key]['MERCHANT'] = $merchant->toArray();
+            //     } else {
+            //         $data[$key]['MERCHANT'] = $merchant;
+            //     }
+
+
+
+            // }
+
+
+
+            // dd($data);
+            return DataTables::of($data)
+
+                ->toJson();
+        }
     }
-}
     public function data2(Request $request)
     {
 
-        
+
         $searchValue = $request['search']['value']; // Search value
         $searchByAmount = $request['searchByAmount'];
         $searchByStatus = $request['searchByStatus'];
         ## Search 
-            $searchQuery = " ";
-            if($searchByAmount != ''){
-               $searchQuery .= " and (emp_name like '%".$searchByAmount."%' ) ";
-            }
-            if($searchByStatus != ''){
-               $searchQuery .= " and (gender='".$searchByStatus."') ";
-            }
-            if($searchValue != ''){
-               $searchQuery .= " and (emp_name like '%".$searchValue."%' or 
-                  email like '%".$searchValue."%' or 
-                  city like'%".$searchValue."%' ) ";
-            }
-
-
-
-
+        $searchQuery = " ";
+        if ($searchByAmount != '') {
+            $searchQuery .= " and (emp_name like '%" . $searchByAmount . "%' ) ";
+        }
+        if ($searchByStatus != '') {
+            $searchQuery .= " and (gender='" . $searchByStatus . "') ";
+        }
+        if ($searchValue != '') {
+            $searchQuery .= " and (emp_name like '%" . $searchValue . "%' or 
+                  email like '%" . $searchValue . "%' or 
+                  city like'%" . $searchValue . "%' ) ";
+        }
     }
 
     public function detail($id)
     {
         $idString = strval($id);
-        // $datas = Http::get('http://192.168.26.26:10002/tm.php')->json();
-        $datas = Transaction::get()->toArray();
-        
+
+        switch (env('APP_ENV')) {
+            case 'local':
+                $datas = Http::get('http://192.168.26.26:10002/tm.php')->json();
+                break;
+            case 'dev':
+                $datas = Http::get('http://192.168.26.26:10002/tm.php')->json();
+                break;
+            case 'prod':
+                $datas = Transaction::get()->toArray();
+                break;
+        }
+
+
+
+
         // $dataDecode = json_decode($datas);
-        
+
         foreach ($datas as $key => $value) {
             // dd($value);
-            
-            $merchant = Merchant::where('ID',$value['MERCHANT_ID'])->first();
+
+            $merchant = Merchant::where('ID', $value['MERCHANT_ID'])->first();
             if ($merchant != '') {
                 // dd($merchant);
                 $datas[$key]['MERCHANT'] = $merchant->toArray();
             } else {
                 $datas[$key]['MERCHANT'] = $merchant;
             }
-            
-            $nns = Nns::where('NNS',$value['ISSUING_INSTITUTION_NAME'])->first();
+
+            $nns = Nns::where('NNS', $value['ISSUING_INSTITUTION_NAME'])->first();
             // dd($nns->toArray());
             if ($nns != '') {
                 // dd($nns);
@@ -171,68 +218,65 @@ class TransactionController extends Controller
                 $datas[$key]['NNS'] = $nns;
             }
 
-            $mpan = MerchantDetails::where('MERCHANT_ID',$value['MERCHANT_ID'])->first();
+            $mpan = MerchantDetails::where('MERCHANT_ID', $value['MERCHANT_ID'])->first();
             if ($mpan != '') {
                 // dd($merchant);
                 $datas[$key]['MPAN'] = $mpan['MPAN'];
             } else {
                 $datas[$key]['MPAN'] = $mpan;
             }
-            
         }
 
-        
-      
-      
-       
+
+
+
+
         foreach ($datas as $data) {
-        
-            if($data['ID'] == $idString )
-            {
 
-            return response()->json([
-       
-                'MERCHANT_ACC_NUMBER'           =>   $data['MERCHANT_ACC_NUMBER'],
-                'TIP_INDICATOR'                 =>   $data['TIP_INDICATOR'],
-                'TRANSFER_REFF'                 =>   $data['TRANSFER_REFF'],
-                'AMOUNT_TIP_PERCENTAGE'         =>   $data['AMOUNT_TIP_PERCENTAGE'],
-                'DESCRIPTION'                   =>   $data['DESCRIPTION'],
-                'QRIS'                          =>   $data['QRIS'],
-                'TRANSACTION_ID'                =>   $data['TRANSACTION_ID'],
-                'STATUS_TRANSFER'               =>   $data['STATUS_TRANSFER'],
-                'STATUS'                        =>   $data['STATUS'],
-                'AMOUNT'                        =>   $data['AMOUNT'],
-                'EXPIRE_DATE_TIME'              =>   $data['EXPIRE_DATE_TIME'],
-                'RETRIEVAL_REFERENCE_NUMBER'    =>   $data['RETRIEVAL_REFERENCE_NUMBER'],
-                'CREATED_AT'                    =>   $data['CREATED_AT'],
-                'TRANSACTION_TYPE'              =>   $data['TRANSACTION_TYPE'],
-                'ID'                            =>   $data['ID'],
-                'FEE_AMOUNT'                    =>   $data['FEE_AMOUNT'],
-                'AMOUNT_REFUND'                 =>   $data['AMOUNT_REFUND'],
-                'INVOICE_NUMBER'                =>   $data['INVOICE_NUMBER'],
-                'POSTAL_CODE'                   =>   $data['POSTAL_CODE'],
-                'UPDATED_AT'                    =>   $data['UPDATED_AT'],
-                'TRANSFER_STATUS'               =>   $data['TRANSFER_STATUS'],
-                'MERCHANT_ID'                   =>   $data['MERCHANT_ID'],
-                'MERCHANT'                      =>   $data['MERCHANT'],
-                'AQUERIER_TYPE'                  =>   $data['AQUERIER_TYPE'],
-                'MID'                              =>   $data['MID'],
-                'RRN_REFUND'                     =>   $data['RRN_REFUND'],
-                'BIT3_RESPONSE'                  =>   $data['BIT3_RESPONSE'],
-                'RC_FUND'                        =>   $data['RC_FUND'],
-                'ACQUIRING_INSTITUTION_NAME'    =>   $data['ACQUIRING_INSTITUTION_NAME'],
-                'ISSUING_INSTITUTION_NAME'      =>   $data['ISSUING_INSTITUTION_NAME'],
-                'ISSUING_CUSTOMER_NAME'         =>   $data['ISSUING_CUSTOMER_NAME'],
-                'CUSTOMER_PAN'                  =>   $data['CUSTOMER_PAN'],
-                'NNS'                  =>   $data['NNS'],
-                'bit_12'                  =>   $data['bit_12'],
-                'BIT_2'                  =>   $data['BIT_2'],
-                'CURRENT_AMOUNT_REFUND'                  =>   $data['CURRENT_AMOUNT_REFUND'],
-                'MPAN'                  =>   $data['MPAN'],
+            if ($data['ID'] == $idString) {
 
-         ]);
-        }}
-        
+                return response()->json([
+
+                    'MERCHANT_ACC_NUMBER'           =>   $data['MERCHANT_ACC_NUMBER'],
+                    'TIP_INDICATOR'                 =>   $data['TIP_INDICATOR'],
+                    'TRANSFER_REFF'                 =>   $data['TRANSFER_REFF'],
+                    'AMOUNT_TIP_PERCENTAGE'         =>   $data['AMOUNT_TIP_PERCENTAGE'],
+                    'DESCRIPTION'                   =>   $data['DESCRIPTION'],
+                    'QRIS'                          =>   $data['QRIS'],
+                    'TRANSACTION_ID'                =>   $data['TRANSACTION_ID'],
+                    'STATUS_TRANSFER'               =>   $data['STATUS_TRANSFER'],
+                    'STATUS'                        =>   $data['STATUS'],
+                    'AMOUNT'                        =>   $data['AMOUNT'],
+                    'EXPIRE_DATE_TIME'              =>   $data['EXPIRE_DATE_TIME'],
+                    'RETRIEVAL_REFERENCE_NUMBER'    =>   $data['RETRIEVAL_REFERENCE_NUMBER'],
+                    'CREATED_AT'                    =>   $data['CREATED_AT'],
+                    'TRANSACTION_TYPE'              =>   $data['TRANSACTION_TYPE'],
+                    'ID'                            =>   $data['ID'],
+                    'FEE_AMOUNT'                    =>   $data['FEE_AMOUNT'],
+                    'AMOUNT_REFUND'                 =>   $data['AMOUNT_REFUND'],
+                    'INVOICE_NUMBER'                =>   $data['INVOICE_NUMBER'],
+                    'POSTAL_CODE'                   =>   $data['POSTAL_CODE'],
+                    'UPDATED_AT'                    =>   $data['UPDATED_AT'],
+                    'TRANSFER_STATUS'               =>   $data['TRANSFER_STATUS'],
+                    'MERCHANT_ID'                   =>   $data['MERCHANT_ID'],
+                    'MERCHANT'                      =>   $data['MERCHANT'],
+                    'AQUERIER_TYPE'                  =>   $data['AQUERIER_TYPE'],
+                    'MID'                              =>   $data['MID'],
+                    'RRN_REFUND'                     =>   $data['RRN_REFUND'],
+                    'BIT3_RESPONSE'                  =>   $data['BIT3_RESPONSE'],
+                    'RC_FUND'                        =>   $data['RC_FUND'],
+                    'ACQUIRING_INSTITUTION_NAME'    =>   $data['ACQUIRING_INSTITUTION_NAME'],
+                    'ISSUING_INSTITUTION_NAME'      =>   $data['ISSUING_INSTITUTION_NAME'],
+                    'ISSUING_CUSTOMER_NAME'         =>   $data['ISSUING_CUSTOMER_NAME'],
+                    'CUSTOMER_PAN'                  =>   $data['CUSTOMER_PAN'],
+                    'NNS'                  =>   $data['NNS'],
+                    'bit_12'                  =>   $data['bit_12'],
+                    'BIT_2'                  =>   $data['BIT_2'],
+                    'CURRENT_AMOUNT_REFUND'                  =>   $data['CURRENT_AMOUNT_REFUND'],
+                    'MPAN'                  =>   $data['MPAN'],
+
+                ]);
+            }
+        }
     }
 }
-
